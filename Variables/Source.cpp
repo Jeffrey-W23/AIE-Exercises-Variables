82903603a4426eded5c01668c77744951d8b0024
Thomas Wiltshire <thomaswiltshire@Kochanski.local>
2017-03-01 15:36:24 +1100

    First Commit.

---
+++ Variables/Source.cpp
@@ -0,0 +1,34 @@
+// Question 4 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int a;
+//	int b;
+//
+//	cin >> a >> b;
+//	cout << a << b;
+//	cout << b << a;
+//}
+// Question 4 //
+
+// Question 7 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	char a;
+//	char b;
+//	char c;
+//
+//	cin >> a >> b;
+//	
+//	c = (a + b) / 2;
+//
+//	cout << c;
+//
+//	system("pause");
+//}
+// Question 7 //
